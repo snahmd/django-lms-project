2eb040913dd1ecdeb5fde4a90a6121dc7a3bbac8
snahmd <sanahmed7@gmail.com>
2023-01-14 23:45:27 +0100

    adding course detail page

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course,Category
 # Register your models here.
 
-admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "available")
+    list_filter= ("available",)
+    search_fields = ("name", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug": ("name",)}
+
--- courses/models.py
+++ courses/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Course(models.Model):
     name= models.CharField(max_length=200, unique=True, verbose_name="Kurs Adi", help_text="Kurs Adini Yaziniz")
     description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
     date = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=True)
