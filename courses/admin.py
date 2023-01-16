e2a8555aa1a0bf3a903c99d646178505951810e5
snahmd <sanahmed7@gmail.com>
2023-01-16 23:50:47 +0100

    adding category and tag attribute

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course,Category
+from .models import Course,Category,Tag
 # Register your models here.
 
 @admin.register(Course)
@@ -13,3 +13,6 @@ class CourseAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields={"slug": ("name",)}
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug": ("name",)}
