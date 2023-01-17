ff5ec934f7d04e0fd68f22ebf8aa101654536b4e
snahmd <sanahmed7@gmail.com>
2023-01-17 23:58:39 +0100

    adding class based views

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from pages.views import AboutView, IndexView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+    # path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
+    # path("about/", views.about, name="about"),
+    path("about/", AboutView.as_view(), name="about"),
 ]
