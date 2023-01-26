58b85a769c65271e62dae7f2933d5e56d73cd4bc
snahmd <sanahmed7@gmail.com>
2023-01-26 23:56:33 +0100

    adding ListView

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from teachers.models import Teacher
 
 # Create your views here.
+
+class TeacherListView(ListView):
+    model = Teacher
+    template_name = "teachers.html"
+    
